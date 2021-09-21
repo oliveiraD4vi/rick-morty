b449dd573ec5bbe622b4753f235a62beabe1590a
Davi Oliveira <davi.comp.oliveira@gmail.com>
2021-09-21 12:35:25 -0300

    [003] elementos basicos de estilo adicionados

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import './styles/global.scss';
 function App() {
   return (
     <div className="App">
-      <h2>Hello, world!</h2>
+      <div className="base-container">
+        <div className="base-showcase"></div>
+
+        <div className="base"></div>
+      </div>
+
+      <div className="lateral-showcase-container">
+        <div className="lateral-showcase"></div>
+
+        <div className="element">
+          by me
+        </div>
+      </div>
     </div>
   );
 }
