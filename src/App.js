5ccf8a16433f61ec4d944dcfc404dead28ef75d1
Davi Oliveira <davi.comp.oliveira@gmail.com>
2021-09-22 14:52:48 -0300

    [010] alinhamento de titulo da pagina e posicionamento dos elementos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import PeopleShowcase from './components/PeopleShowcase';
 import PeopleInfoShowcase from './components/PeopleInfoShowcase';
 
+import { MenuOutlined } from '@ant-design/icons/lib/icons';
+
 import { PeopleInfoProvider } from './context/PeopleContext';
 import { PageProvider } from './context/PageContext';
 
@@ -13,6 +15,10 @@ function App() {
         <div className="App">
           <div className="base-container">
             <div className="base-showcase">
+              <div className="identifier">
+                <h1>Rick and Morty</h1>
+                <MenuOutlined />
+              </div>
               <PeopleInfoShowcase />
             </div>
 
